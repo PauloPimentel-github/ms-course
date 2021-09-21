de4e0d552d5a27b80b4c055f30261f3b1e34d3dd
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2021-09-21 14:18:19 -0300

    hr-worker imnplementation

---
+++ hr-worker/src/main/java/com/phpimentel/hrworker/entities/Worker.java
@@ -0,0 +1,23 @@
+package com.phpimentel.hrworker.entities;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double dailyIncome;
+
+}
