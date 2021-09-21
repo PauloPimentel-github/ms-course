dac695d4ee89c0ba314316e73fa0954cf8141cad
Paulo Pimentel <paulo.h.g.pimentel@gmail.com>
2021-09-21 16:58:06 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/phpimentel/hrpayroll/entities/dto/Worker.java
@@ -0,0 +1,17 @@
+package com.phpimentel.hrpayroll.entities.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Worker implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private Double dailyIncome;
+}
--- hr-payroll/src/main/java/com/phpimentel/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/phpimentel/hrpayroll/services/PaymentService.java
@@ -1,12 +1,31 @@
 package com.phpimentel.hrpayroll.services;
 
 import com.phpimentel.hrpayroll.entities.Payment;
+import com.phpimentel.hrpayroll.entities.dto.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String hrWorkerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+        Map<String, String> params = new HashMap<>();
+        params.put("id", workerId.toString());
+        StringBuilder url = new StringBuilder(this.hrWorkerHost);
+        url.append("/workers/{id}");
+
+        Worker worker = this.restTemplate.getForObject(url.toString(), Worker.class, params);
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
